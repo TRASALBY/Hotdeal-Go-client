48146d83fd5322e43decd6913c80005b81841150
이호성 <ghtjd8264@naver.com>
2023-12-06 22:16:37 +0900

    feat: 카카오 로그인 기능 구현

--- app/src/main/java/com/budge/hotdeal_go/presentation/view/LoginFragment.kt
+++ app/src/main/java/com/budge/hotdeal_go/presentation/view/LoginFragment.kt
@@ -1,11 +1,20 @@
 package com.budge.hotdeal_go.presentation.view
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.budge.hotdeal_go.R
 import com.budge.hotdeal_go.databinding.FragmentLoginBinding
 import com.budge.hotdeal_go.presentation.base.BaseFragment
 import com.budge.hotdeal_go.presentation.viewmodel.LoginViewModel
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.AndroidEntryPoint
+
+private const val TAG = "LoginFragment"
 
 @AndroidEntryPoint
 class LoginFragment : BaseFragment<FragmentLoginBinding>(
@@ -13,6 +22,52 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(
     R.layout.fragment_login
 ) {
 
-    private val viewModel : LoginViewModel by viewModels()
+    private val viewModel: LoginViewModel by viewModels()
+
+    val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if (error != null) {
+            Log.e(TAG, "카카오계정으로 로그인 실패", error)
+        } else if (token != null) {
+            Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setEvent()
+    }
+
+    private fun setEvent() {
+        binding.ibKakaoLogin.setOnClickListener {
+            kakaoLogin()
+        }
+    }
+
+    private fun kakaoLogin() {
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(requireContext())) {
+            UserApiClient.instance.loginWithKakaoTalk(requireContext()) { token, error ->
+                if (error != null) {
+                    Log.e(TAG, "카카오톡으로 로그인 실패", error)
+
+                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+
+                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                    UserApiClient.instance.loginWithKakaoAccount(
+                        requireContext(),
+                        callback = callback
+                    )
+                } else if (token != null) {
+                    Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = callback)
+        }
+    }
+
 
 }
