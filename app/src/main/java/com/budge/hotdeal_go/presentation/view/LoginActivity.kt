43a06a69bed5e22e8226c2dc603ab381ab5e2e82
이호성 <ghtjd8264@naver.com>
2023-12-06 14:05:49 +0900

    feat: LoginActivity 임시 디자인

---
+++ app/src/main/java/com/budge/hotdeal_go/presentation/view/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.budge.hotdeal_go.presentation.view
+
+import com.budge.hotdeal_go.databinding.ActivityLoginBinding
+import com.budge.hotdeal_go.presentation.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
+
+
+}
