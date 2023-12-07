d9beb1f5b91e2c291e425776aa5feb785df5dd93
serot <serotonins023@gmail.com>
2023-12-07 18:06:11 +0900

    HotDeal fragment searchItem

--- app/src/main/java/com/budge/hotdeal_go/presentation/view/HotDealFragment.kt
+++ app/src/main/java/com/budge/hotdeal_go/presentation/view/HotDealFragment.kt
@@ -1,12 +1,16 @@
 package com.budge.hotdeal_go.presentation.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.budge.hotdeal_go.R
 import com.budge.hotdeal_go.databinding.FragmentHotDealBinding
 import com.budge.hotdeal_go.presentation.base.BaseFragment
+import com.budge.hotdeal_go.presentation.viewmodel.HotDealViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
+private const val TAG = "HotDealFragment"
 
 @AndroidEntryPoint
 class HotDealFragment : BaseFragment<FragmentHotDealBinding>(
@@ -14,9 +18,20 @@ class HotDealFragment : BaseFragment<FragmentHotDealBinding>(
     R.layout.fragment_hot_deal
 ) {
 
+    private val viewModel by viewModels<HotDealViewModel>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setObserve()
 
+        viewModel.searchItem(booleanArrayOf(false, true, true))
     }
+
+    private fun setObserve() {
+        viewModel.hotDealItemList.observe(viewLifecycleOwner) {
+            Log.d(TAG, "onViewCreated: $it")
+        }
+    }
+
 
 }
--- app/src/main/java/com/budge/hotdeal_go/presentation/viewmodel/HotDealViewModel.kt
+++ app/src/main/java/com/budge/hotdeal_go/presentation/viewmodel/HotDealViewModel.kt
@@ -1,11 +1,15 @@
 package com.budge.hotdeal_go.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.budge.hotdeal_go.data.model.HotDealItem
 import com.budge.hotdeal_go.domain.usecase.GetFmkoreaHotdealItems
 import com.budge.hotdeal_go.domain.usecase.GetQuasarzoneHotdealItems
 import com.budge.hotdeal_go.domain.usecase.GetRuliwebHotdealItems
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,22 +20,48 @@ class HotDealViewModel @Inject constructor(
     private val getRuliwebHotdealItems: GetRuliwebHotdealItems
 ) : ViewModel() {
 
-    fun getFmKoreaHotdeal(){
-        viewModelScope.launch {
-            getFmkoreaHotdelItems.getItems()
-        }
-    }
+    private val _hotDealItemList = MutableLiveData<List<HotDealItem>>(emptyList())
+    val hotDealItemList: LiveData<List<HotDealItem>> get() = _hotDealItemList
 
-    fun getQuasarzoneHotdeal(){
-        viewModelScope.launch {
-            getQuasarzoneHotdealItems.getItems()
+    private fun getFmKoreaHotdeal() =
+        viewModelScope.async {
+            runCatching {
+                getFmkoreaHotdelItems.getItems()
+            }.getOrDefault(emptyList())
         }
-    }
 
-    fun getRuliwebHotdeal(){
-        viewModelScope.launch {
+
+    private fun getQuasarzoneHotdeal() =
+        viewModelScope.async {
+            runCatching {
+                getQuasarzoneHotdealItems.getItems()
+            }.getOrDefault(emptyList())
+        }
+
+
+    private fun getRuliwebHotdeal() = viewModelScope.async {
+        runCatching {
             getRuliwebHotdealItems.getItems()
+        }.getOrDefault(emptyList())
+    }
+
+
+    fun searchItem(booleanArray: BooleanArray) {
+
+        val newHotDealItemList = mutableListOf<HotDealItem>()
+        viewModelScope.launch {
+            booleanArray.forEachIndexed { index, boolean ->
+                if (boolean) {
+                    when (index) {
+                        0 -> newHotDealItemList.addAll(getFmKoreaHotdeal().await())
+                        1 -> newHotDealItemList.addAll(getQuasarzoneHotdeal().await())
+                        2 -> newHotDealItemList.addAll(getRuliwebHotdeal().await())
+                    }
+                }
+            }
         }
+
+        _hotDealItemList.value = newHotDealItemList.sortedBy { it.time }
     }
 
 
