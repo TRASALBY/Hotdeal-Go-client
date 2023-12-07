55efe0d7d911321e6d0ed967b7cfcc1c033baf83
이호성 <ghtjd8264@naver.com>
2023-12-07 13:32:38 +0900

    feat: 어플리케이션에서 Prefs 추가

--- app/src/main/java/com/budge/hotdeal_go/core/HotDealApplication.kt
+++ app/src/main/java/com/budge/hotdeal_go/core/HotDealApplication.kt
@@ -2,6 +2,7 @@ package com.budge.hotdeal_go.core
 
 import android.app.Application
 import com.budge.hotdeal_go.BuildConfig
+import com.budge.hotdeal_go.core.util.EncryptedPrefs
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
@@ -10,5 +11,6 @@ class HotDealApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+        EncryptedPrefs.initSharedPreferences(applicationContext)
     }
 }
