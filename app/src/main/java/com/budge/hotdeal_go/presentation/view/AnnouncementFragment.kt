446a487b0b467eaebcafa02b660f58ad9747e148
이호성 <ghtjd8264@naver.com>
2023-12-13 15:06:59 +0900

    feat: 공지 페이지 디자인, 페이지 버튼 기능

--- app/src/main/java/com/budge/hotdeal_go/presentation/view/AnnouncementFragment.kt
+++ app/src/main/java/com/budge/hotdeal_go/presentation/view/AnnouncementFragment.kt
@@ -2,9 +2,11 @@ package com.budge.hotdeal_go.presentation.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.budge.hotdeal_go.R
 import com.budge.hotdeal_go.databinding.FragmentAnnouncementBinding
 import com.budge.hotdeal_go.presentation.base.BaseFragment
+import com.lakue.pagingbutton.OnPageSelectListener
 
 class AnnouncementFragment : BaseFragment<FragmentAnnouncementBinding>(
     FragmentAnnouncementBinding::inflate,
@@ -13,6 +15,25 @@ class AnnouncementFragment : BaseFragment<FragmentAnnouncementBinding>(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.lpbButtonlist.setPageItemCount(4)
+        binding.lpbButtonlist.addBottomPageButton(10,1)
+        binding.lpbButtonlist.setOnPageSelectListener(object : OnPageSelectListener{
+            override fun onPageBefore(nowPage: Int) {
+                binding.lpbButtonlist.addBottomPageButton(10, nowPage)
+                Toast.makeText(requireContext(), nowPage.toString(), Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onPageCenter(nowPage: Int) {
+                Toast.makeText(requireContext(), nowPage.toString(), Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onPageNext(nowPage: Int) {
+                binding.lpbButtonlist.addBottomPageButton(10, nowPage)
+                Toast.makeText(requireContext(), nowPage.toString(), Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 
 }
