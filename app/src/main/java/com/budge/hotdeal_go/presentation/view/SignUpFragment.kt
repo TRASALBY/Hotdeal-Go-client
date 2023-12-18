48687b2a3bc3430aa123c7a24bed249084bbc5cd
이호성 <ghtjd8264@naver.com>
2023-12-19 02:09:26 +0900

    feat: 임시 회원가입 화면

---
+++ app/src/main/java/com/budge/hotdeal_go/presentation/view/SignUpFragment.kt
@@ -0,0 +1,12 @@
+package com.budge.hotdeal_go.presentation.view
+
+import com.budge.hotdeal_go.R
+import com.budge.hotdeal_go.databinding.FragmentSignUpBinding
+import com.budge.hotdeal_go.presentation.base.BaseFragment
+
+class SignUpFragment : BaseFragment<FragmentSignUpBinding>(
+    FragmentSignUpBinding::inflate,
+    R.layout.fragment_sign_up
+) {
+
+}
