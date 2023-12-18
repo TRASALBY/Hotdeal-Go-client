d44d38948083e828b46eeec5d9cf71f64b5922b2
이호성 <ghtjd8264@naver.com>
2023-12-19 01:31:18 +0900

    feat: 토큰 관련 인터셉터 분리

--- app/src/main/java/com/budge/hotdeal_go/core/di/ApiModule.kt
+++ app/src/main/java/com/budge/hotdeal_go/core/di/ApiModule.kt
@@ -9,15 +9,41 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ApiModule {
 
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class HotDealRetrofit
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class AuthRetrofit
+
     @Singleton
     @Provides
-    fun provideRetrofit(
+    fun provideHotDealRetrofit(
+        @NetworkModule.HotDealClient
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        val builder = Retrofit.Builder()
+        builder
+            .baseUrl("http://13.125.124.61:80/")
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+
+        return builder.build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideAuthRetrofit(
+        @NetworkModule.AuthClient
         okHttpClient: OkHttpClient
     ): Retrofit {
         val builder = Retrofit.Builder()
@@ -32,12 +58,14 @@ object ApiModule {
     @Singleton
     @Provides
     fun provideHotDealAPI(
+        @HotDealRetrofit
         retrofit: Retrofit
     ): HotDealApi = retrofit.create(HotDealApi::class.java)
 
     @Singleton
     @Provides
     fun provideMemberAPI(
+        @AuthRetrofit
         retrofit: Retrofit
     ): MemberApi = retrofit.create(MemberApi::class.java)
 }
--- app/src/main/java/com/budge/hotdeal_go/core/di/NetworkModule.kt
+++ app/src/main/java/com/budge/hotdeal_go/core/di/NetworkModule.kt
@@ -4,14 +4,25 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class HotDealClient
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class AuthClient
+
+    @HotDealClient
     @Singleton
     @Provides
     fun provideOkHTTPClient(): OkHttpClient {
@@ -22,6 +33,35 @@ object NetworkModule {
             addInterceptor(loggingInterceptor)
         }
         return builder.build()
+    }
+
+    @AuthClient
+    @Singleton
+    @Provides
+    fun provideAuthClient(
+        authorizationInterceptor: Interceptor
+    ): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        builder.apply {
+            addInterceptor(loggingInterceptor)
+            addInterceptor(authorizationInterceptor)
+        }
+        return builder.build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesAuthorizationInterceptor() = Interceptor { chain ->
+        val request = chain.request().newBuilder()
+        val hasAuthorization = chain.request().headers.names().contains("Authorization")
+
+        if (hasAuthorization) {
+            val accessToken = chain.request().header("Authorization")
+            request.header("Authorization", "Bearer $accessToken")
+        }
+        chain.proceed(request.build())
     }
 
 }
